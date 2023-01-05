1a3c16f4956d1e2423e9cbcda05f9934e9b82fda
yutteee <yusaku_nakamura_aa@icloud.com>
2023-01-05 11:32:14 +0900

    簡単にディレクトリ構成整えた #2

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,7 +1,2 @@
-export default function Home() {
-  return (
-    <div>
-      <p>Hello, World!</p>
-    </div>
-  )
-}
+import { HomePage } from "../features/home/HomePage"
+export default HomePage;
