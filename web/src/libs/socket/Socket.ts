16783ad04ef50881950ba41ae2afc4dd0310275b
yutteee <yusaku_nakamura_aa@icloud.com>
2023-02-13 00:05:39 +0900

    socketで使う関数をクラスに全て追加 #14

--- web/src/libs/socket/Socket.ts
+++ web/src/libs/socket/Socket.ts
@@ -1,13 +1,18 @@
 import { io, Socket } from 'socket.io-client';
+import { CardType } from '../types/Card';
 
 interface ServerToClientEvents {
-  noArg: () => void;
-  basicEmit: (a: number, b: string, c: Buffer) => void;
-  withAck: (d: string, callback: (e: number) => void) => void;
+  pushPlayPage: (room_id: String, player1_id: number, player2_id: number) => void; //FullRoom
+  updateField: (card_data: Array<CardType>, player_id: number) => void; //HPinfo
+  gameStart: (player_name: String) => void; // gameStart
 }
 
 interface ClientToServerEvents {
-  hello: () => void;
+  matching: (player_id: number) => void; //AutoMatchingPreLogin
+  quitWaitingRoom: (player_id: number) => void; // LeaveWaitingRoom
+  enterWaitingRoom: (room_id: String) => void; //login
+  joinRoom: (room_id: String, opponent_id: number) => void; // roomJoin
+  sendCard: (card_data: Array<CardType>, player_id: number) => void; //cardValue
 }
 
 class SocketIo {
@@ -19,6 +24,44 @@ class SocketIo {
       'https://nl2nzrkpqe.execute-api.ap-northeast-1.amazonaws.com/CODE_DUEL_mock/@connections'
     );
     console.log('Connecting Socket.io...');
+  }
+
+  enterWaitingRoom(room_id: String) {
+    this.socket?.emit('enterWaitingRoom', room_id);
+  }
+
+  matching(player_id: number) {
+    this.socket?.emit('matching', player_id);
+  }
+
+  quitWaitingRoom(player_id: number) {
+    this.socket?.emit('quitWaitingRoom', player_id);
+  }
+
+  pushPlayPage(callback: (room_id: String, player1_id: number, player2_id: number) => void) {
+    this.socket?.on('pushPlayPage', (room_id: String, player1_id: number, player2_id: number) => {
+      return callback(room_id, player1_id, player2_id);
+    });
+  }
+
+  joinRoom(room_id: String, opponent_id: number) {
+    this.socket?.emit('joinRoom', room_id, opponent_id);
+  }
+
+  gameStart(callback: (player_name: String) => void) {
+    this.socket?.on('gameStart', (player_name: String) => {
+      return callback(player_name);
+    });
+  }
+
+  sendCard(card_data: Array<CardType>, player_id: number) {
+    this.socket?.emit('sendCard', card_data, player_id);
+  }
+
+  updateField(callback: (card_data: Array<CardType>, player_id: number) => void) {
+    this.socket?.on('updateField', (card_data: Array<CardType>, player_id: number) => {
+      return callback(card_data, player_id);
+    });
   }
 }
 
