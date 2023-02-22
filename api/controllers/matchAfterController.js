48ff0a514cb7563ed393510214511052e565d0fe
YutaK1026 <inheritans1026@gmail.com>
2023-02-22 14:27:48 +0900

    add-マッチ終了後のAPI処理を記述

--- api/controllers/matchAfterController.js
+++ api/controllers/matchAfterController.js
@@ -56,7 +56,17 @@ module.exports = {
     },
     sendResult: async(req, res) => {
         try{
-            
+            const user_id = req.body.user_id;
+            const is_win = req.body.result;
+            let exp = 0;
+            await db.Task.findOne({where:{user_id: user_id}}).then(user => {
+                exp = user.exp;
+            })
+            exp += 10 + is_win*10
+            await db.Task.update(
+                {exp: exp},
+                {where:{id: user_id}})
+            const result = [user_id, exp]
             res.send(result);
         }catch(err){
             res.status(500).send(err);
--- api/routes/matchBeforeRoutes.js
+++ api/routes/matchBeforeRoutes.js
@@ -4,20 +4,14 @@ matchBeforeController = require("../controllers/matchBeforeController");
 router.get("/", function(req, res){
     matchBeforeController.read(req, res)
 });
-router.post("/", function(req, res){
-    matchBeforeController.create(req, res)
-});
 router.put("/:id", function(req, res){
     matchBeforeController.update(req, res)
-});
-router.delete("/:id", function(req, res){
-    matchBeforeController.delete(req, res)
 });
 
 router.post("/custom_match", function(req, res){
     matchBeforeController.customMatch(req, res)
 });
-router.post("/random_match", function(req, res){
+router.get("/random_match", function(req, res){
     matchBeforeController.randomMatch(req, res)
 });
 router.post("/delete_room", function(req, res){
