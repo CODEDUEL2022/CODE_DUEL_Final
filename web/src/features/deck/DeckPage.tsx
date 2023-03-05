4da184f04dcc3c58163a19b9825dce36644954cc
yutteee <yusaku_nakamura_aa@icloud.com>
2023-03-05 14:33:20 +0900

    デッキ選択ボタンを作成 #26

--- web/src/features/deck/DeckPage.tsx
+++ web/src/features/deck/DeckPage.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from 'react';
+import { MainButton } from '../../components/parts/Button/MainButton';
+import { Modal } from '../../components/parts/Modal/Modal';
 
 export const DeckPage = () => {
-    return(
-        <div>デッキ選択画面です</div>
-    )
-}
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openModal = function () {
+    setIsModalOpen(true);
+  };
+  const closeModal = function () {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div>
+      <MainButton handleClick={openModal}>
+        <div
+          style={{
+            height: '30px',
+            fontSize: '20px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            textTransform: 'none',
+          }}
+        >
+          デッキ選択
+        </div>
+      </MainButton>
+      <Modal isModalOpen={isModalOpen} onClick={closeModal}>
+        <h1>デッキを選択しよう</h1>
+      </Modal>
+    </div>
+  );
+};
