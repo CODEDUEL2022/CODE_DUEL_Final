cd40fb257839ca11f5d0b54e610f086f84b51cd6
yutteee <yusaku_nakamura_aa@icloud.com>
2023-03-26 02:34:25 +0900

    カードをトランプのように広げた #32

--- web/src/features/play/templates/FieldCards.tsx
+++ web/src/features/play/templates/FieldCards.tsx
@@ -15,17 +15,33 @@ export const FieldCards: React.FC<CardProps> = (props) => {
     <>
       <div className="card-container">
         {cards?.map((card) => (
-          <FieldCard
-            card={card}
-            isActive={ableUseCardIds.some((id) => id === card.id)}
-            onClick={handleClick}
-          ></FieldCard>
+          <div className="card">
+            <FieldCard
+              card={card}
+              isActive={ableUseCardIds.some((id) => id === card.id)}
+              onClick={handleClick}
+            ></FieldCard>
+          </div>
         ))}
       </div>
       <style jsx>{`
         .card-container {
           display: flex;
-          gap: 10px;
+        }
+        .card:nth-child(1) {
+          transform: rotate(-30deg) translate(40px, 70px);
+        }
+        .card:nth-child(2) {
+          transform: rotate(-15deg) translate(30px, 20px);
+        }
+        .card:nth-child(3) {
+          transform: rotate(0deg);
+        }
+        .card:nth-child(4) {
+          transform: rotate(15deg) translate(-30px, 20px);
+        }
+        .card:nth-child(5) {
+          transform: rotate(30deg) translate(-40px, 70px);
         }
       `}</style>
     </>
