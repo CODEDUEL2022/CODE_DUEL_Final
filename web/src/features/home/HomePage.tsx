6988b37c0f5632d9af9e42518a85abbc55e237dc
yutteee <yusaku_nakamura_aa@icloud.com>
2023-04-21 21:43:07 +0900

    :+1: cssを入れ子にした #45

--- web/src/features/home/HomePage.tsx
+++ web/src/features/home/HomePage.tsx
@@ -19,44 +19,47 @@ export const HomePage = () => {
           background: linear-gradient(180deg, rgb(2, 5, 8, 100%), rgb(20, 79, 97, 100%));
           height: 100svh;
           width: 100%;
-        }
-
-        .titles {
-          text-align: center;
-          height: 60%;
-        }
-
-        .subtitle {
-          color: #fff;
-          font-size: 1.5cqw;
-          font-weight: 100;
-          letter-spacing: 0.1cqw;
-          font-family: Gill Sans, sans-serif;
-          padding-top: 8%;
-          padding-bottom: 4%;
-        }
-
-        .title {
-          color: #fff;
-          font-size: 5cqw;
-          letter-spacing: 1cqw;
-          font-weight: 100;
-          font-family: Gill Sans, sans-serif;
-        }
-
-        .signin {
-          color: #fff;
-          font-size: 2.5cqw;
-          letter-spacing: 0.3cqw;
-          text-decoration: none;
-          font-weight: 100;
-          font-family: Gill Sans, sans-serif;
-        }
-
-        .auth {
-          text-align: center;
-          height: 40%;
-          margin-top: 5%;
+          .titles {
+            text-align: center;
+            height: 60%;
+            .subtitle {
+              color: #fff;
+              font-size: 1.5cqw;
+              font-weight: 100;
+              letter-spacing: 0.1cqw;
+              font-family: Gill Sans, sans-serif;
+              padding-top: 8%;
+              padding-bottom: 4%;
+              text-shadow: 0 0 5px #d3fffd;
+            }
+            .title {
+              color: #fff;
+              font-size: 5cqw;
+              letter-spacing: 1cqw;
+              font-weight: 100;
+              font-family: Gill Sans, sans-serif;
+              text-shadow: 0 0 10px #d3fffd;
+            }
+          }
+          .auth {
+            height: 40%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            .signin {
+              color: #fff;
+              font-size: 2.5cqw;
+              letter-spacing: 0.3cqw;
+              text-decoration: none;
+              font-weight: 100;
+              font-family: Gill Sans, sans-serif;
+              text-shadow: 0 0 5px #d3fffd;
+              transition: all 0.4s;
+            }
+            .signin:hover {
+              text-shadow: 0 0 20px #d3fffd;
+            }
+          }
         }
       `}</style>
     </>
