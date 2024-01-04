8486ec3f6bc1994e7b0e6d5b02985d890ab5f148
yutteee <yusaku_nakamura_aa@icloud.com>
2024-01-04 22:24:18 +0900

    user -> player #75

--- schema/socket.ts
+++ schema/socket.ts
@@ -1,8 +1,8 @@
 interface ServerToClientEvents {
   successRandomMatching: (
     room_id: number,
-    user1_id: number,
-    user2_id: number
+    player1_id: number,
+    player2_id: number
   ) => void;
   updateField: (
     round: number,
@@ -12,18 +12,18 @@ interface ServerToClientEvents {
     cardsData: Array<CardType>,
     playersData: Array<PlayerType>
   ) => void;
-  startGame: (turn: number, user1: PlayerType, user2: PlayerType) => void;
+  startGame: (turn: number, player1: PlayerType, player2: PlayerType) => void;
   finishGame: (winner: number, loser: number /* player id **/) => void;
 }
 
 interface ClientToServerEvents {
-  enterWaitingRoom: (user_id: number, user_name: string) => void;
-  exitWaitingRoom: (user_id: number, user_name: string) => void;
-  enterPlayingRoom: (room_id: number, user: PlayerType) => void;
-  exitPlayingRoom: (room_id: number, user: PlayerType) => void;
+  enterWaitingRoom: (player_id: number, player_name: string) => void;
+  exitWaitingRoom: (player_id: number, player_name: string) => void;
+  enterPlayingRoom: (room_id: number, player: PlayerType) => void;
+  exitPlayingRoom: (room_id: number, player: PlayerType) => void;
   sendCards: (
     room_id: number,
-    user_id: number,
+    player_id: number,
     combo: ComboType | null,
     cards: Array<CardType>
   ) => void;
