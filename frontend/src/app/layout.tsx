2d845ba47df3c2d7c8670fd0bae799b08b32e452
yutteee <yusaku_nakamura_aa@icloud.com>
2024-02-16 17:07:12 +0900

    storybookにglobal cssを定義 #78

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google';
 import type { Metadata } from 'next';
-import './globals.scss';
+import '@/styles/globals.scss';
 
 const inter = Inter({ subsets: ['latin'] });
 
