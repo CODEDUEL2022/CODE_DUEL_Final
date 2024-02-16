57bd2b4888df34b8a03435cc675870480ddb301e
kawachan <inheritans1026@gmail.com>
2024-02-16 21:01:50 +0900

    change-DBテーブル名変更-OsをOSに変更

--- db/init/init.sql
+++ db/init/init.sql
@@ -3,7 +3,7 @@ CREATE DATABASE codeduel;
 -- 作成したDBに接続
 \c codeduel;
 
-INSERT INTO "Os" VALUES(1, 'testOS', 'testPath')
+INSERT INTO "OS" VALUES(1, 'testOS', 'testPath')
 INSERT INTO "Card" VALUES(1, 1, 'sample1', 100, 'ATTACK', 'hoge');
 INSERT INTO "Card" VALUES(2, 1, 'sample2', 200, 'HEAL', 'fuga');
 INSERT INTO "Card" VALUES(3, 1, 'sample3', 300, 'ABSORPTION', 'huge');
