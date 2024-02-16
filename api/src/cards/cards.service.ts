711a914d6936d299ee1b096629fd1ebc9476de47
kawachan <inheritans1026@gmail.com>
2024-02-16 14:53:05 +0900

    add-DBからcard情報を取得できるように修正

--- api/src/cards/cards.service.ts
+++ api/src/cards/cards.service.ts
@@ -1,29 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { GetCardInterface } from './interfaces/cards.interfaces';
 import { resolve } from 'path';
+import { PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient({})
 @Injectable()
 export class CardsService {
-    findOne(id: number): Promise<GetCardInterface> {
-        const testValue: GetCardInterface = {
-            name: "testCard",
-            value: 100,
-            type: 'ATTACK',
-            path: 'test'
-        }
+    findOne(id: number): Promise<GetCardInterface[]> {
+        const card = prisma.card.findMany({
+            where: {id: Number(id)}
+        })
         return new Promise(resolve => {
-            resolve(testValue)
+            resolve(card)
         })
     }
     findAll(): Promise<GetCardInterface[]>{
-        const testValue: GetCardInterface = {
-            name: "testCard",
-            value: 100,
-            type: 'ATTACK',
-            path: 'test'
-        }
+        const cards = prisma.card.findMany({})
         return new Promise(resolve => {
-            resolve([testValue])
+            resolve(cards)
         })
     }
 }
--- api/src/cards/interfaces/cards.interfaces.ts
+++ api/src/cards/interfaces/cards.interfaces.ts
@@ -1,4 +1,6 @@
 export interface GetCardInterface{
+    id: number
+    os_id: number
     name: string
     value: number
     type: 'ATTACK' | 'HEAL' | 'ABSORPTION'
