10479b3d455ccb81c077e00ce783378649865a40
yutteee <yusaku_nakamura_aa@icloud.com>
2024-04-20 21:51:56 +0900

    hooksを作成 #97

--- frontend/src/features/DeckCarousel/index.tsx
+++ frontend/src/features/DeckCarousel/index.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import { DeckCarouselPresentation } from './presentations/';
+import { useDeckCarousel } from './hooks/';
 
 type Props = {};
 
 export const DeckCarousel: React.FC<Props> = (props) => {
-  return <DeckCarouselPresentation />;
+  const { deckName, cards, handlePrevDeck, handleNextDeck } = useDeckCarousel();
+
+  return (
+    <DeckCarouselPresentation
+      deckName={deckName}
+      cards={cards}
+      handlePrevDeck={handlePrevDeck}
+      handleNextDeck={handleNextDeck}
+    />
+  );
 };
--- frontend/src/features/DeckCarousel/presentations/index.tsx
+++ frontend/src/features/DeckCarousel/presentations/index.tsx
@@ -3,18 +3,29 @@ import styled from './index.module.scss';
 import { MdNavigateBefore } from 'react-icons/md';
 import { MdNavigateNext } from 'react-icons/md';
 import { Button } from '@/ui/Button';
+import { Cards } from '../../../../api/@types';
 
-type Props = {};
+type Props = {
+  deckName?: string;
+  cards?: Cards;
+  handlePrevDeck: () => void;
+  handleNextDeck: () => void;
+};
 
-export const DeckCarouselPresentation: React.FC<Props> = (props) => {
+export const DeckCarouselPresentation: React.FC<Props> = ({
+  deckName,
+  cards,
+  handlePrevDeck,
+  handleNextDeck,
+}) => {
   return (
     <div className={styled.container}>
       <div className={styled.titleWrapper}>
-        <button className={styled.moveButton}>
+        <button className={styled.moveButton} onClick={handlePrevDeck}>
           <MdNavigateBefore size={24} />
         </button>
-        <div className={styled.title}>JS Deck</div>
-        <button className={styled.moveButton}>
+        <div className={styled.title}>{deckName}</div>
+        <button className={styled.moveButton} onClick={handleNextDeck}>
           <MdNavigateNext size={24} />
         </button>
       </div>
@@ -83,7 +94,7 @@ export const DeckCarouselPresentation: React.FC<Props> = (props) => {
           height={48}
         />
       </div>
-      <Button size="sm" text="JS Deckを選択" />
+      <Button size="sm" text={`${deckName}を選択`} />
     </div>
   );
 };
