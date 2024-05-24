514e63fd4de83dfb6cb851f4f73972fd3031711a
yutteee <yusaku_nakamura_aa@icloud.com>
2024-05-24 21:26:16 +0900

    アクセシビリティ改善 #105

---
+++ frontend/src/features/ModalButton/hooks/index.ts
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+
+type useModalButton = () => {};
+
+export const useModalButton: useModalButton = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return {};
+};
--- frontend/src/features/ModalButton/presentations/index.tsx
+++ frontend/src/features/ModalButton/presentations/index.tsx
@@ -10,7 +10,7 @@ type Props = {
 export const ModalButtonPresentation: React.FC<Props> = ({ icon: Icon, text }) => {
   return (
     <div>
-      <button className={styled.button}>
+      <button className={styled.button} aria-label={`${text}ダイアログを開く`}>
         <Icon size={24} className={styled.icon} />
         <span className={styled.text}>{text}</span>
       </button>
